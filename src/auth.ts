9797a3b47198e14c348ceff5d0f0c252fc2652a4
Ayaman Tai <167556938+taiayman@users.noreply.github.com>
2026-01-05 20:05:20 +0100

    fix: Force Google account picker on every login

--- src/auth.ts
+++ src/auth.ts
@@ -10,6 +10,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      },
     }),
   ],
   trustHost: true,
